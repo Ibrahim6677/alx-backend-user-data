21f952b6a3e9ec430577bc0eb049a69cba9f0be1
ibrahim <ibrahimsaad6677@gmail.com>
2024-09-19 13:24:30 +0300

    Update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,7 +3,6 @@
 Route module for the API
 """
 from os import getenv
-from api.v1 import auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -21,40 +20,6 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-@app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
-    """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
